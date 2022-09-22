5ae4c369c76bd1fa375669d8484d252308b419a9
Rafael Vieira Rodrigues <100781229+IRafael-VieiraI@users.noreply.github.com>
2022-09-22 13:30:34 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function displaynum(n1) {
+  Calculator.text1.value = Calculator.text1.value + n1; 
+}
+/* Função que pega o valor do texto 1 e adiciona o texto1 + n1 que no caso é o que guardamos dentro das funções.
+ */
